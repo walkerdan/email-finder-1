f0a5f121e79604dfe64214d5e8bb371065f37bcb
brianraster <brianaster85@gmail.com>
2020-05-08 21:31:01 -0500

    commit1

---
+++ src/test/java/edu/depaul/email/EmailFinderTest.java
@@ -0,0 +1,28 @@
+package edu.depaul.email;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EmailFinderTest {
+  @Test
+  @DisplayName("Test test")
+  void Test() {
+    int a = 1+2;
+  }
+  @Test
+  @DisplayName("Test that a new StorageService is an instance of StorageService")
+  void testSetupStorage() {
+    StorageService t = new StorageService();
+    assertTrue(t instanceof StorageService);
+  }
+  @Test
+  @DisplayName("a")
+
+}
