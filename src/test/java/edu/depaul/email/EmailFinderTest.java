12634c2230362671222cd4f2521f37d2b6e265fc
brianraster <brianaster85@gmail.com>
2020-05-12 09:04:04 -0500

    5/12 commit

--- src/test/java/edu/depaul/email/EmailFinderTest.java
+++ src/test/java/edu/depaul/email/EmailFinderTest.java
@@ -1,5 +1,7 @@
 package edu.depaul.email;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,4 +20,13 @@ public class EmailFinderTest {
     assertTrue(t instanceof StorageService);
   }
 
+  @Test
+  @DisplayName("Test Page Parser")
+  void testPageParser() {
+    String html = "<html><a href='/some/other/file.html'>my link</a></body></html>";
+    Document doc = Jsoup.parse(html);
+    PageParser test = new PageParser();
+    test.findLinks(doc);
+    assertEquals(null,doc.getElementById("my link"));
+  }
 }
